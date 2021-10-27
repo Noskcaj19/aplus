0de69c56d4a9fb7a4ba48a16a94721b352bac088
Noskcaj <kcajdev@gmail.com>
2021-10-27 12:51:47 -0400

    Make course-id optional

--- README.md
+++ README.md
@@ -7,12 +7,11 @@ This tool lets you view your A+ attendance and submit codes if you can access A+
     Usage: aplus.py [OPTIONS] CODE
 
     Options:
-      -s, --show            Show attendance data
       -t, --token TEXT      Canvas LMS API token generated in settings  [required]
       -u, --base-url TEXT   Base url to your Canvas instance  [required]
-      -c, --course-id TEXT  Course id to any course with A+ attendance enable
-                            [required]
-
+      -c, --course-id TEXT  Course id to any course with A+ attendance enable,
+                            optional but faster if provided
+      -s, --show            Show attendance data
       --help                Show this message and exit.
 
 An API token can be created at `<canvas-base-url>/profile/settings` under "Approved Integrations:".
--- aplus.py
+++ aplus.py
@@ -7,8 +7,10 @@ import colorama
 from aplus.aplus import APlus, NoAvailableCodesException
 
 
-def get_aplus(canvas, course_id: str) -> Optional[APlus]:
-    for tool in canvas.get_course(course_id).get_external_tools(include_parents=True):
+def get_aplus(canvas, course_id: Optional[str]) -> Optional[APlus]:
+    for tool in canvas.get_course(
+        course_id or canvas.get_courses()[0].id
+    ).get_external_tools(include_parents=True):
         if "aPlus" not in tool.name:
             continue
         return APlus(tool)
@@ -30,14 +32,6 @@ def show_attendance_callback(ctx: click.Context, _p: click.Option, v: bool):
 
 @click.command()
 @click.option(
-    "-s",
-    "--show",
-    is_flag=True,
-    callback=show_attendance_callback,
-    is_eager=True,
-    help="Show attendance data",
-)
-@click.option(
     "-t",
     "--token",
     required=True,
@@ -51,9 +45,16 @@ def show_attendance_callback(ctx: click.Context, _p: click.Option, v: bool):
 @click.option(
     "-c",
     "--course-id",
-    required=True,
     type=str,
-    help="Course id to any course with A+ attendance enable",
+    help="Course id to any course with A+ attendance enable, optional but faster if provided",
+)
+@click.option(
+    "-s",
+    "--show",
+    is_flag=True,
+    callback=show_attendance_callback,
+    is_eager=True,
+    help="Show attendance data",
 )
 @click.argument("code")
 def aplus(show: bool, token: str, base_url: str, course_id: str, code: str):
